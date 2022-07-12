892718478930061b91144db1f6efd0fdb4adc093
tejas273 <bentenison1098@gmail.com>
2022-07-12 23:14:29 +0530

    first commit

---
+++ src/components/ProductReviews.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ProductReviewsCard from "./ProductReviewCard.js"
+import "../styles/ProductReviews.css"
+
+const ProductReviews = ({productReviews}) => {
+  return (
+    <div className='productReviews'>
+        {productReviews.map((item,index)=>(
+            <ProductReviewsCard price={item.price} name={item.name} image={item.image} review={item.review} index={index} key={item.image} />
+        ))}
+    </div>
+  )
+}
+
+export default ProductReviews
